2a3db8187b4fa68ca2b5716ee3a2b262bd4ac977
Pongsakorn2341 <meepanda2341@gmail.com>
2023-03-25 15:10:06 +0700

    add engine

---
+++ engine/cn332-arial-car-track/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+rm -rf runs/
+python3 -m pip install -r requirements.txt
+# python3 main.py --weights yolov7.pt --source "video2.mp4"
+# python3 detect.py --weights yolov7.pt --source "video2.mp4"
